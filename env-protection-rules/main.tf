327e75f9dfb9ab7a3d63a1e5b4caf2f28c43894e
SathuSid <sisindhriy123@gmail.com>
2025-02-08 22:01:45 +0530

    argoCD

---
+++ env-protection-rules/main.tf
@@ -0,0 +1,53 @@
+provider "github" {
+  owner = "siddevsecops007"
+}
+
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
+variable "repos" {
+  default = {
+    "roboshop-cart"      = {}
+    "roboshop-catalogue" = {}
+    "roboshop-user"      = {}
+    "roboshop-shipping"  = {}
+    "roboshop-payment"   = {}
+    "roboshop-frontend"  = {}
+  }
+}
+
+variable "env" {
+  default = {
+    "E2E-TESTS"  = {}
+    "QA"   = {}
+    "UAT"  = {}
+    "PROD" = {}
+  }
+}
+
+locals {
+  repos_with_envs    = { for i, j in var.repos : i => { for x, y in var.env : "${i}_${x}" => { "env" = x, "app" = i } } }
+  repo_with_env_list = flatten([for a, b in local.repos_with_envs : values(b)])
+}
+
+data "github_user" "user" {
+  username = "SathuSid"
+}
+
+resource "github_repository_environment" "env" {
+  count               = length(local.repo_with_env_list)
+  environment         = local.repo_with_env_list[count.index]["env"]
+  repository          = local.repo_with_env_list[count.index]["app"]
+  prevent_self_review = false
+  reviewers {
+    users = [data.github_user.user.id]
+  }
+}
